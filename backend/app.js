05466f6a642de367e8faab684fe5583fba7fd473
rasel-stacklearner <rasel@stacklearner.com>
2023-10-08 17:48:05 +0600

    🔧 chore(app.js): update CORS configuration to allow specific origins, methods, and headers for improved security and compatibility 🔧 chore(app.js): update Keycloak configuration to use environment variables and improve flexibility for different environments 🔧 chore(app.js): update server listening function to handle errors and log them for debugging purposes

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,13 @@ var cors = require('cors');
 
 var app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:8000","http://localhost:8180"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 var memoryStore = new session.MemoryStore();
 
@@ -16,16 +22,35 @@ app.use(session({
   store: memoryStore
 }));
 
-var keycloak = new Keycloak({ store: memoryStore });
+var keycloak = new Keycloak(
+  // { store: memoryStore },
+  {},
+  {
+    "client-id":"mydemo",
+    realm: "demo",
+    // "bearer-only": true,
+    "auth-server-url": "http://localhost:8180/auth",
+    "ssl-required": "external",
+  //   resource: "mydemo",
+  //   secret: "201b2f63-0ed7-4a35-9f4a-5c433c328b72",
+  //   realmPublicKey:
+  //     "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4HB3LEon3djpnjgIGUYyfTF1Z3Fe7HqMDZXoZzmI3YLRguuQCOc7pCuacqr20nIzCYyUQ/EyGyXzAZ+wKluOzXULPT9VitV15086gJmsen1Nn8+MzqPqFrVpollldHe5qCEMm1GQa1VjU7xZlQvKX0t03yL84Ps8mauvmdZ1qn5lUc9CBQRQLFeWx2vk7Ivsn83Ukx6/wG9QZ6x//0eJJnJe7gHs/WEIy+v0hDqYBlbNStSptB4m8Ht0dH8HcbetTxrVjcSRpscNWgrgns6f7f/w4+HkkmKeD0yf3ZR2GKhQRnb1fvvjdvaSQFFDKt5ko/goptrhX53WwjebrmYLOQIDAQAB",
+  //   "client-id": "mydemo",
+  }
+);
 
 app.use(keycloak.middleware());
 
-app.get('/secured', keycloak.protect('realm:myrole'), function (req, res) {
-  res.setHeader('content-type', 'text/plain');
-  res.send('Secret message!');
+app.get("/secured", keycloak.protect("realm:myrole"), function (req, res) {
+// app.get("/secured",  function (req, res) {
+  console.log(req.headers)
+  console.log(keycloak)
+  res.setHeader("content-type", "text/plain");
+  res.send("Secret message!");
 });
 
 app.get('/public', function (req, res) {
+  console.log(req.header)
   res.setHeader('content-type', 'text/plain');
   res.send('Public message!');
 });
@@ -34,6 +59,7 @@ app.get('/', function (req, res) {
   res.send('<html><body><ul><li><a href="/public">Public endpoint</a></li><li><a href="/secured">Secured endpoint</a></li></ul></body></html>');
 });
 
-app.listen(3000, function () {
+app.listen(3000, function (err) {
   console.log('Started at port 3000');
+  console.log(err)
 });
